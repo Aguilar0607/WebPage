ac9ea5a5d7eefcda627c0ff113e9e9f3e905a191
Aguilar0607 <emmanuelaguilar0607@gmail.com>
2023-06-30 15:14:41 -0600

    manejando .php

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
 <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="/sitio/Paguina/WebPage/index" class="flex items-center mb-4 sm:mb-0">
+            <a href="/sitio/Paguina/WebPage/index.php" class="flex items-center mb-4 sm:mb-0">
                 <img src="/sitio/Paguina/WebPage/assets/img/logos/olpega-logo-blue.svg" class="h-8 mr-3" alt="Olpega Logo" />
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
