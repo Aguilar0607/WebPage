884ec5204788048ff2627188cac5f9b35638b3dd
Aguilar0607 <emmanuelaguilar0607@gmail.com>
2023-06-30 09:52:54 -0600

    Prueba commit 2

--- templates/contact.php
+++ templates/contact.php
@@ -36,7 +36,8 @@
                 <div class="bg-gray-300 rounded-lg overflow-hidden p-10 relative h-full">
                     <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map"
                         marginheight="0" marginwidth="0" scrolling="no"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1258.5736496686736!2d-100.28805627047292!3d25.773403855641668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866293616f1ae0d7%3A0xd5283dbadfe3ce0d!2sDistribuidora%20Olpega!5e0!3m2!1ses-419!2smx!4v1687972751291!5m2!1ses-419!2smx"></iframe>
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1258.5736496686736!2d-100.28805627047292!3d25.773403855641668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866293616f1ae0d7%3A0xd5283dbadfe3ce0d!2sDistribuidora%20Olpega!5e0!3m2!1ses-419!2smx!4v1687972751291!5m2!1ses-419!2smx">
+                    </iframe>
                 </div>
             </div>
             <div class="py-8 ">
